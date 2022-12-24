239102659bc4f18da42c136068afdf3cf800afae
paolofullone <paolo.enrico@gmail.com>
2022-12-24 10:39:41 -0300

    feat: :sparkles: Person Interface

---
+++ Presentation.Domain/Interfaces/IPerson.cs
@@ -0,0 +1,18 @@
+﻿using Presentation.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Presentation.Domain.Interfaces
+{
+    public interface IPerson
+    {
+        Task<IEnumerable<Person>> GetPersonsAsync();
+        Task<Person> GetPersonAsync(int id);
+        Task<Person> AddPersonAsync(Person person);
+        Task<Person> UpdatePersonAsync(Person person);
+        Task<Person> RemovePersonAsync(int id);
+    }
+}
