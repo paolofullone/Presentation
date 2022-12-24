0762a01b52246ea7a652116dddf903c59ac7f42f
paolofullone <paolo.enrico@gmail.com>
2022-12-24 11:07:09 -0300

    refactor: :recycle: inclusion of linkedin url

--- Presentation.Domain/Entities/Person.cs
+++ Presentation.Domain/Entities/Person.cs
@@ -8,21 +8,22 @@ namespace Presentation.Domain.Entities
         public string Email { get; private set; } = string.Empty;
         public string City { get; private set; } = string.Empty;
         public string State { get; private set; } = string.Empty;
+        public string LinkedinUrl { get; private set; } = string.Empty;
         public DateTime BirhtDate { get; private set; }
 
-        public Person(string fullName, string email, string city, string state, DateTime birhtDate)
+        public Person(string fullName, string email, string city, string state, string linkedinUrl, DateTime birhtDate)
         {
-            ValidateDomain(fullName, email, city, state, birhtDate);
+            ValidateDomain(fullName, email, city, state, linkedinUrl, birhtDate);
         }
 
-        public Person(int id, string fullName, string email, string city, string state, DateTime birhtDate)
+        public Person(int id, string fullName, string email, string city, string state, string linkedinUrl, DateTime birhtDate)
         {
             DomainExceptionValidation.When(id < 0, "Invalid id value");
             Id = id;
-            ValidateDomain(fullName, email, city, state, birhtDate);
+            ValidateDomain(fullName, email, city, state, linkedinUrl, birhtDate);
         }
 
-        private void ValidateDomain(string fullName, string email, string city, string state, DateTime birhtDate)
+        private void ValidateDomain(string fullName, string email, string city, string state, string linkedinUrl, DateTime birhtDate)
         {
             DomainExceptionValidation.When(string.IsNullOrEmpty(fullName), "The name is required");
             DomainExceptionValidation.When(fullName.Length < 3, "The name is too short, minimum 3 characters");
@@ -31,13 +32,17 @@ namespace Presentation.Domain.Entities
             DomainExceptionValidation.When(string.IsNullOrEmpty(city), "The city is required");
             DomainExceptionValidation.When(city.Length < 3, "The city is too short, minimum 3 characters");
             DomainExceptionValidation.When(string.IsNullOrEmpty(state), "The state is required");
-            DomainExceptionValidation.When(state.Length < 3, "The state is too short, minimum 3 characters");
+            DomainExceptionValidation.When(state.Length > 2, "The state is too long, maximum 2 characters");
+            DomainExceptionValidation.When(linkedinUrl?.Length > 0 && !Uri.IsWellFormedUriString(linkedinUrl, UriKind.Absolute), "The linkedin url is invalid");
+            DomainExceptionValidation.When(birhtDate > DateTime.Now, "The birth date is greater than the current date");
+
             DomainExceptionValidation.When(birhtDate > DateTime.Now, "The birthdate is invalid");
 
             FullName = fullName;
             Email = email;
             City = city;
             State = state;
+            LinkedinUrl = linkedinUrl!;
             BirhtDate = birhtDate;
         }
     }
