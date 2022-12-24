352f11af23eae5623f8de913faaa9b4d5db4fd89
paolofullone <paolo.enrico@gmail.com>
2022-12-24 15:07:59 -0300

    feat: :card_file_box: dependency injection, migration and database update

--- Presentation.Api/Program.cs
+++ Presentation.Api/Program.cs
@@ -1,4 +1,6 @@
 
+using CleanArchMvc.Infra.IoC;
+
 namespace Presentation.Api
 {
     public class Program
@@ -13,6 +15,9 @@ namespace Presentation.Api
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            // Add Infrastructure
+            builder.Services.AddInfrastructure(builder.Configuration);
 
             var app = builder.Build();
 
---
+++ Presentation.Infra.IoC/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Presentation.Domain.Interfaces;
+using Presentation.Infra.Data.Context;
+using Presentation.Infra.Data.Repository;
+
+namespace CleanArchMvc.Infra.IoC
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(configuration.GetConnectionString("MSSqlConnection"),
+            b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            // segudo Macoratti a recomendação para aplicações web é AddScoped
+            services.AddScoped<IPersonRepository, PersonRepository>();
+
+            return services;
+
+        }
+    }
+}
